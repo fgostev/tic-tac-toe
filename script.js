cc89934f5e7844b0779735da09d685c60497457d
Filipp Gostev <fgostev@gmail.com>
2021-07-05 12:47:12 -0500

    added working winer function(no styling) and restart button

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ const gameBoard = (() => {
         square.textContent = board[i];
     }
 
+
     return { board, squareArr };
 })();
 
 const createPlayer = (name, figure) => {  
     return{ name, figure}
+
 }
 
+
+
 const game = (() => {
+
 
     
     const Player1 = createPlayer("Player1", "x");
@@ -31,29 +36,23 @@ const game = (() => {
 
     let round = 1;
 
-
-    let player1Moves = [];
-    let player2Moves = [];
-
-    // FIGURE OUT HOW TO COMPARE THE MOVES TO THE WINS!!!
-
     function turn(){
 
         if(round % 2 === 0){
             this.textContent = Player2.figure;
             gameBoard.board.splice(this.id, 1, Player2.figure);
             this.disabled = true;
-            player2Moves.push(this.id);
+            player2Moves.push(parseInt(this.id));
             player2Moves.sort();
         }
         else{
             this.textContent = Player1.figure;
             gameBoard.board.splice(this.id, 1, Player1.figure);
             this.disabled = true;
-            player1Moves.push(this.id);
+            player1Moves.push(parseInt(this.id));
             player1Moves.sort();
         }
-
+        gameWinner();
         round++;
 
             console.log("x" + " " + player1Moves);
@@ -62,6 +61,88 @@ const game = (() => {
     }
 
 
+let player1Moves = [];
+let player2Moves = [];
+
+// array from player moves to compare with the win results
+
+    function gameChecker(array1, array2){
+
+        let newArr = [];
+    
+            array2.forEach(array2Val =>{
+                
+                array1.forEach(array1Val => {
+                    if(array2Val === array1Val){
+                        newArr.push(array2Val);
+                    }else{
+                        return newArr;
+                    }
+                })
+            })
+            return newArr;
+    }
+
+    // determine the winner
+    
+    let player1Win = false;
+    let player2Win = false;
+    let tie = false;
+    
+    function gameWinner(){
+
+    const winner = document.getElementById("winner");
+    
+        const winningLines = [
+                    [0, 1, 2],
+                    [3, 4, 5],
+                    [5, 6, 7],
+                    [0, 3, 6],
+                    [1, 4, 7],
+                    [2, 5, 8],
+                    [0, 4, 8],
+                    [2, 4, 6]
+                ]
+    
+        winningLines.forEach(line =>{
+           if(gameChecker(line, player1Moves).length === 3){
+            player1Win = true;
+            winner.textContent = "Player 1 WIN!"
+            }
+            else if(gameChecker(line, player2Moves).length === 3){
+                player2Win = true;
+                winner.textContent = "Player 2 WIN!"
+            }else if(player1Moves.length > 4 && player2Moves.length > 3){
+                tie = true;
+                winner.textContent = "TIE!"
+            }
+
+    })
+
+}
+
+// restart button
+
+const restart = document.getElementById("restart");
+
+function restartBoard(){
+    console.log("test");
+    gameBoard.board = ["","","","","","","","",""];
+    player1Moves = [];
+    player2Moves = [];
+    player1Win = false;
+    player2Win = false;
+    tie = false;
+    round = 1;
+
+    gameBoard.squareArr.forEach(square =>{
+        square.textContent = "";
+        square.disabled = false;
+    })
+}
+
+restart.addEventListener('click', restartBoard);
+
     gameBoard.squareArr.forEach(square => {
         square.addEventListener("click", turn)
     })
@@ -69,115 +150,8 @@ const game = (() => {
 
 })();
 
+// for today:
+// restart button
+// modal for winner
+// modal for settings and player customization
 
-// function testArr(){
-
-//     const winningLines = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [5, 6, 7],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6]
-//     ]
-
-//     let countTrue = 0;
-//     let arr2 = [0,1,2];
-
-//     winningLines.forEach(line =>{
-
-//         // console.log(line);
-
-//         line.forEach(ele =>{f
-//             if(line[ele] === arr2[ele]){
-//                         countTrue++;
-//                     }
-//         })
-//     })
-
-//     console.log(countTrue);
-
-// }
-// testArr();
-
-// check every
-
-// if for array first element doesn't equals second array and length is longer - remove first index
-
-
-let win = null;
-
-// function to compare arrays
-
-
-
-// function testArr(){
-
-//         const winningLines = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [5, 6, 7],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6]
-//     ]
-
-//     // if winning line is less then the players length add "" and sort both of them so they match?
-//     // how to check if number doesn't matches?
-
- 
-//     let b = [2, 4, 6]; 
-//     let newArr = [];
-
-
-//         winningLines.forEach((line) => {
-
-//             if(equals(line, b))
-//                 console.log(true);
-//         })
-//     };
-
-// // figure out a way to check arrays longer then 3.
-
-// // Try to remove not matching numbers to get the same length?
-
-
-function brainStorm(){
-    const a = [0, 2, 4];
-    const b = [0, 2, 4];
-
-    // this is the representation of gameboard array
-    const c = [0, 1, 2];
-
-    // representation of the possible win result
-    let newArr = [];
-
-
-const equals = (array1, array2) =>
-    array1.every((v, i) => v === array2[i]);    
-
-        c.forEach(valC =>{
-            
-            a.forEach(valA => {
-                if(valC === valA){
-                    newArr.push(valC);
-                }else{
-                    return newArr;
-                }
-            })
-        })
-    // console.log(newArr);
-        return {newArr};
-    console.log(equals(a, newArr))
-}
-
-
-
-
-
-// const equals = (array1, array2) =>
-//     array1.length === array2.length && array1.every((v, i) => v === array2[i]);
